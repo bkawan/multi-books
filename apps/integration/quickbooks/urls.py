33979a8df7e337b0d0badaa57dc4874de9fdfe7e
bkawan <ibikeshkawan@gmail.com>
2025-12-09 00:52:43 +0545

    refactor integration

--- apps/integration/quickbooks/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-from django.http import HttpResponse
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("login/", views.connect_to_quickbooks, name="quickbooks_login"),
-    path("callback/", views.quickbooks_callback, name="quickbooks_callback"),
-]
--- apps/integration/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('zoho/', include('apps.integration.zoho.urls')),
-    path('quickbooks/', include('apps.integration.quickbooks.urls')),
-]
